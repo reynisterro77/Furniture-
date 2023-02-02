ae4e9d6b7df751475927b235ce78a087e27c5d08
Melih Kara <79717410+reynisterro77@users.noreply.github.com>
2023-02-02 18:09:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Menu
+let menu=document.querySelector('.menu-icon');
+let navbar=document.querySelector('.navbar');
+menu.onclick=()=>{
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+    cart.classList.remove('active');
+    login.classList.remove('active');
+}
+
+
+// Cart Toggle
+let cart=document.querySelector('.cart');
+document.querySelector('#cart-icon').onclick=()=>{
+    cart.classList.toggle('active');
+    navbar.classList.remove('active');
+    menu.classList.remove('move');
+    login.classList.remove('active')
+}
+
+// Login Form Toggle
+let login =document.querySelector('.login-form');
+document.querySelector('#user-icon').onclick=()=>{
+    login.classList.toggle('active')
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+    menu.classList.remove('move');
+}
+
+
+// Change Header Backgroun Color And Shadow On Scroll
+let header=document.querySelector('header');
+window.addEventListener('scroll',()=>{
+    header.classList.toggle('shadow',window.scrollY>0);
+});
+
+//Scroll Top
+let scrollTop=document.querySelector('.scroll-top');
+window.addEventListener('scroll',()=>{
+    scrollTop.classList.toggle('active',window.scrollY>0);
+});
+
+// On Click On menu links Removed Menu
+window.onscroll=()=>{
+    navbar.classList.remove('active');
+    menu.classList.remove('move');
+}
+
+
+
+
